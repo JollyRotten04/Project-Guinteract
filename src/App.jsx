b4e057f67d06f482eda6aacd55c8c5ed37bda420
Vince Justine C. Bas <vincebasb4@gmail.com>
2024-07-26 12:36:59 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DeleteLater from './components/DeleteLater'
+
+function App() {
+  return (
+    <div className="App">
+      <DeleteLater></DeleteLater>
+      {/* <h1>Hello World!</h1> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/DeleteLater.jsx
@@ -0,0 +1,5 @@
+import './styles/DeleteLaterStyles.css';
+
+export default function DeleteLater(){
+    return <h1>Hello World!</h1>
+}

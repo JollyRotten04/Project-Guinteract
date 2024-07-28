cc2d0a1eb2762b37ac2d08edf430ce4a13764224
Vince Justine C. Bas <vincebasb4@gmail.com>
2024-07-28 11:31:09 +0800

    fixed some things

--- src/frontend/view/components/Logo_Component/LogoComponent.jsx
+++ src/frontend/view/components/Logo_Component/LogoComponent.jsx
@@ -2,7 +2,7 @@ export default function LogoComponent(){
     return(
         <div className = "logo">
             <p id = "logoFirstPart">Guinter</p>
-            <img src="/src/assets/1.png" alt="" id="logoPickIcon" />
+            <img src="../../../assets/1.png" alt="" id="logoPickIcon" />
             <p id="logoSecondPart">ct</p>
         </div>
     );

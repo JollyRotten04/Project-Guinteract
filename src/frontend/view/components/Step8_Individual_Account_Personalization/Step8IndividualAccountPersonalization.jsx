892622e379c8a6378ed03ba74e23f344bd76b15c
Vince Justine C. Bas <vincebasb4@gmail.com>
2024-08-24 13:46:19 +0800

    Fixed step 8, but still needs adjusting

--- src/frontend/view/components/Step8_Individual_Account_Personalization/Step8IndividualAccountPersonalization.jsx
+++ src/frontend/view/components/Step8_Individual_Account_Personalization/Step8IndividualAccountPersonalization.jsx
@@ -1,50 +1,87 @@
+import React, { useRef, useState, useEffect } from 'react';
 import './Step8IndividualAccountPersonalizationStyles.css';
-export default function Step8IndividualAccountPersonalization(){
-    return(
+
+export default function Step8IndividualAccountPersonalization() {
+    const accountRefs = useRef([]); // Initialize an array to store refs
+    const [addedCounter, setAddedCounter] = useState(0); // Use useState for addedCounter
+    const [isPortrait, setIsPortrait] = useState(window.matchMedia("(orientation: portrait)").matches);
+
+    const handleAccountClick = (event, index) => {
+        const buttonClassList = event.currentTarget.classList;
+
+        if (!buttonClassList.contains('clicked')) {
+            buttonClassList.add('clicked');
+            setAddedCounter((prevCounter) => prevCounter + 1); // Increment the counter
+        } else {
+            buttonClassList.remove('clicked');
+            setAddedCounter((prevCounter) => prevCounter - 1); // Decrement the counter
+        }
+    };
+
+    useEffect(() => {
+        const updateOrientation = () => {
+            setIsPortrait(window.matchMedia("(orientation: portrait)").matches);
+        };
+
+        // Add event listener to track orientation changes
+        window.addEventListener('resize', updateOrientation);
+
+        return () => {
+            window.removeEventListener('resize', updateOrientation);
+        };
+    }, []);
+
+    return (
         <div className="step8IndividualAccountPersonalization">
             <p id="mainLabel">Lastly, please follow at least 5 other accounts...</p>
 
             <div id="innerContainer">
-                
-                {/* Create dynamic creator for options of accounts */}
-
-                {/* PLACEHOLDER VALUES */}
                 <div id="accountsContainer">
-                    <div className="account">
-                        <div id="imgContainer">
-                            <img src = "../../assets/" alt="" id="guitarDefaultIcon" />
-                        </div>
-                    </div>
+                    {[...Array(6)].map((_, index) => (
+                        <div
+                            key={index}
+                            className="account"
+                            ref={(el) => (accountRefs.current[index] = el)} // Assign the ref
+                        >
+                            {/* Image Container */}
+                            <div id="imgContainer">
+                                <img
+                                    src="../../assets/GuitarDefaultIcon.png"
+                                    alt=""
+                                    id="guitarDefaultIcon"
+                                    className="profileIcon"
+                                />
+                            </div>
 
-                    <div className="account">
-                        <div id="imgContainer">
-                            
-                        </div>
-                    </div>
+                            <div id="rightContainer">
+                            {isPortrait ? (
+                                <div id="portraitMode">
+                                    {/* Account Name */}
+                                    <p className="accountName">Placeholder {index + 1}</p>
 
-                    <div className="account">
-                        <div id="imgContainer">
-                            
-                        </div>
-                    </div>
+                                    {/* Number of Friends */}
+                                    <p className="friendCount">5m friends</p>
+                                </div>
+                            ) : (
+                                <>
+                                    {/* Account Name */}
+                                    <p className="accountName">Placeholder {index + 1}</p>
 
-                    <div className="account">
-                        <div id="imgContainer">
-                            
-                        </div>
-                    </div>
+                                    {/* Number of Friends */}
+                                    <p className="friendCount">5m friends</p>
+                                </>
+                            )}
 
-                    <div className="account">
-                        <div id="imgContainer">
-                            
+                                {/* Button Container */}
+                                <button
+                                    className="addFriendButton"
+                                    onClick={(event) => handleAccountClick(event, index)} // Pass event and index
+                                >
+                                    ADD FRIEND
+                                </button>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="account">
-                        <div id="imgContainer">
-                            
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
